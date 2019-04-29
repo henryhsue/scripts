98ef66942d5ec137e91afe879c84caf81f1d76cd
Henry Hsue <hhenry@uber.com>
2019-04-29 20:45:38 +0800

    open empty session if henry session exists

--- new_session.sh
+++ new_session.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-tmux new-session -d -s henry
-tmux select-pane -t 0
-tmux send-keys 'cd /Users/hhenry/work/work-notes' 'Enter' 'vim TasksDaily' 'Enter' ':' 'set nu!' 'Enter' 'G'
-tmux send-keys ':set nonumber' 'Enter'
-tmux rename-window 'notes'
-tmux split-window -h -t henry
-tmux select-pane -t 1
-tmux send-keys 'cd /Users/hhenry/work/work-notes' 'Enter' 'vim NotesDaily' 'Enter' ':' 'set nu!' 'Enter' 'G'
-tmux send-keys ':set nonumber' 'Enter' 
-tmux a -t henry
-
-
+tmux ls | grep henry
+if [ $(echo $?) ]
+then
+    tmux new-session
+else
+    name="henry"
+    tmux new-session -d -s henry
+    tmux select-pane -t 0
+    tmux send-keys 'cd /Users/henryhsue/Personal/notes' 'Enter' 'vim TasksDaily' 'Enter' ':' 'set nu!' 'Enter' 'G'
+    tmux send-keys ':set nonumber' 'Enter'
+    tmux rename-window 'notes'
+    tmux split-window -h -t henry
+    tmux select-pane -t 1
+    tmux send-keys 'cd /Users/henryhsue/Personal/notes' 'Enter' 'vim NotesDaily' 'Enter' ':' 'set nu!' 'Enter' 'G'
+    tmux send-keys ':set nonumber' 'Enter' 
+    tmux a -t henry
+fi
